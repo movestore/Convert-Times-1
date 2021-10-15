4a5fa569a32124effab0d9627c691a656d686af2
Andrea Kölzsch <65662928+andreakoelzsch@users.noreply.github.com>
2021-10-15 10:26:51 +0200

    first version

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# Convert-Times
+# Convert Times
+
+MoveApps
+
+Github repository: *github.com/movestore/Convert-Times*
+
+## Description
+This App converts the data timestamps to (i) local timezones (with savings time), (ii) details of local times, (iii) mean solar time and/or (iv) true solar time. The values are added to the output and can be downloaded as .csv artefact. 
+
+## Documentation
+All timestamps of your data are (by selection) converted to local timezones with savings time at the locations of the data. Timezones are extracted from longitute and latitude by the tz_lookup_coords() function of the lutz package. For each local timestamp it is also possible to have the following time components extracted: date, time, year, month, Julian day, calender week, weekday.
+
+It is also possible to extract solar times of the data's timestamps. It is possible to select for mean or true solar times. Mean solar time is approximated by adjustment of 3.989 min per degree from longitute zero. It approximates solar noon at clock noon to ~20 min accuracy. True solar time is then calculated from mean solar time by use of the equation of time (see Yard et al. 2005. Ecological Modelling). The two solar time conversions are adapted from the function convert_UTC_to_solartime() in the R-package "USGS-R/streamMetabolizer" by Alison Appling.
+
+### Input data
+moveStack in Movebank format
+
+### Output data
+moveStack in Movebank format
+
+### Artefacts
+`data_wtime.csv`: csv-file of the complete data set with the selected converted timestamps added.
+
+### Parameters 
+`local`: Checkbox to select if timestamps shall be converted to local times and added to the dataset. Defaults to FALSE.
+
+`local_details`: Checkbox to select if time components of the converted local times shall be added to the dataset. Defaults to FALSE.
+
+`mean_solar`: Checkbox to select if timestamps shall be converted to mean solar time and added to the dataset. Defaults to FALSE.
+
+`true_solar`: Checkbox to select if timestamps shall be converted to true solar time and added to the dataset. Defaults to FALSE.
+
+### Null or error handling:
+**Parameter `local`:** The default value FALSE will lead to no local times being added to the dataset. NULL is not possible.
+
+**Parameter `local_detail`:** The default value FALSE will lead to no local time components being added to the dataset. NULL is not possible.
+
+**Parameter `mean_solar`:** The default value FALSE will lead to no mean solar time being added to the dataset. NULL is not possible.
+
+**Parameter `true_solar`:** The default value FALSE will lead to no true solar time being added to the dataset. NULL is not possible.
+
+**Data:** The input dataset is not changed, but possibly extended by some attributes. If no time conversions are requested, the input dataset is returned. It cannot be empty.
---
+++ RFunction.R
@@ -0,0 +1,81 @@
+library('move')
+library('lubridate')
+library('lutz')
+
+rFunction <- function(data,local=FALSE,local_details=FALSE,mean_solar=FALSE,true_solar=FALSE)
+{
+
+  Sys.setenv(tz="UTC")
+  
+  if (local==FALSE & local_details==FALSE & mean_solar==FALSE & true_solar==FALSE) logger.info("You have not selected any timestamp conversion. The original dataset will be returned and provided as csv output.")
+  
+  data.csv <- as.data.frame(data)
+  names(data.csv) <- make.names(names(data.csv),allow_=FALSE)
+  data.csv <- data.csv[c("trackId","timestamp","location.long","location.lat","sensor","taxon.canonical.name")]
+  
+  # ask the cache and/or Google for the timezone at these coordinates
+  tz_info <- tz_lookup_coords(coordinates(data)[,2], coordinates(data)[,1], method = "accurate")
+  
+  #return in daylight local
+  if(local==TRUE) 
+  {
+    logger.info("You have selected to add local timestamps.")
+    timestamp_local <- apply(data.frame(timestamps(data),tz_info), 1, function(x) as.character(lubridate::with_tz(x[1], x[2])))
+    data@data <- cbind(data@data,timestamp_local,"local_lz"=tz_info)
+    data.csv <- cbind(data.csv,"local_lz"=tz_info)
+  }
+  if (local_details==TRUE)
+  {
+    logger.info("You have selected to add detailed time information of the local timestamps.")
+    timestamp_local <- apply(data.frame(timestamps(data),tz_info), 1, function(x) as.character(lubridate::with_tz(x[1], x[2])))
+    date <- as.Date(timestamp_local)
+    
+    time <- strftime(timestamp_local, format="%H:%M:%S")
+    year <- year(timestamp_local)
+    month <- months(as.POSIXct(timestamp_local))
+    yday <- as.numeric(strftime(date, format = "%j"))
+    calender_week <- as.numeric(strftime(date, format = "%V"))
+    weekday <- weekdays(as.POSIXct(timestamp_local))
+    data@data <- cbind(data@data,date,time,year,month,weekday,yday,calender_week)
+    data.csv <- cbind(data.csv,date,time,year,month,weekday,yday,calender_week)
+  }
+  if (mean_solar==TRUE)
+  {
+    logger.info("You have selected to add mean solar time timestamps.")
+    # reference to convert_UTC_to_solartime function by Alison Appling (USGS)
+    time.adjustment <- 239.34 * coordinates(data)[,1] #in secs --> 3.989 minutes = 239.34 seconds per degree
+    timestamp_mean_solar <- as.character(timestamps(data) + as.difftime(time.adjustment,units="secs")) #add seconds to the UTC time
+    data@data <- cbind(data@data,timestamp_mean_solar)
+    data.csv <- cbind(data.csv,timestamp_mean_solar)
+  }
+  if (true_solar==TRUE)
+  {
+    logger.info("You ahve selected to add true solar time timestamps.")
+    time.adjustment <- 239.34 * coordinates(data)[,1] #in secs --> 3.989 minutes = 239.34 seconds per degree
+    timestamp_mean_solar <- timestamps(data) + as.difftime(time.adjustment,units="secs") #add seconds to the UTC time
+    # Use the equation of time to compute the discrepancy between apparent and
+    # mean solar time. E is in minutes.
+    jday <- as.numeric(strftime(timestamp_mean_solar, format = "%j")) -1
+    E <- 9.87*sin(((2*360*(jday-81))/365)/180*pi) - 7.53*cos(((360*(jday-81))/365)/180*pi) - 1.5*sin(((360*(jday-81))/365)/180*pi)
+    timestamp_true_solar <- timestamp_mean_solar + as.difftime(E,units="mins")
+    data@data <- cbind(data@data,timestamp_true_solar)
+    data.csv <- cbind(data.csv,timestamp_true_solar)
+  }    
+
+  write.csv(data.csv, file = paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),"data_wtime.csv"),row.names=FALSE)
+  #write.csv(data.csv, file = "data_wtime.csv",row.names=FALSE)
+  
+  result <- data
+  return(result)
+}
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
